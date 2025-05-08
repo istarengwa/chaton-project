59a3085dd63ce8479bfdba95bf80b2f36fd19596
istarengwa <muller.lilian@outlook.fr>
2025-05-08 15:08:32 +0200

    add new buttons edit / delete and add items for admin

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :carts, through: :cart_items
-  has_many :order_items
+  has_many :order_items, dependent: :destroy
   has_many :orders, through: :order_items
 
 
