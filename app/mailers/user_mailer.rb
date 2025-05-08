93b5854775a73d6204a59c047edd6c2d0595446c
istarengwa <muller.lilian@outlook.fr>
2025-05-08 14:50:14 +0200

    add admin and mail for admin

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,14 @@ class UserMailer < ApplicationMailer
     @url = 'http://monsite.fr/login'
     mail(to: @user.email, subject: 'Bienvenue chez nous !')
   end
+
+  def order_notification(order)
+    @order = order
+    @user = order.user
+  
+    # Récupère tous les emails d'admin
+    admins = User.where(is_admin: true).pluck(:email)
+  
+    mail(to: admins, subject: "Nouvelle commande passée !")
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_05_125138) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_08_120404) do
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id", null: false
     t.integer "item_id", null: false
@@ -63,6 +63,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_05_125138) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
